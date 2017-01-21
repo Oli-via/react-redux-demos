3b370491774c2ed3f5d0b299bcb55b9f79d9d9a1
Olivia Wang <aoli.wl@alibabapictures.com>
2017-01-21 16:19:06 +0800

    更改input-value和create-post两个reducers的结构

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import createCommentReducer from './create_comment_reducer'
 import inputValueReducer from './input_value_reducer'
 
+// 把单个数据处理逻辑放在单个reducer里，不要混杂放在同个reducer的同个对象里
 const rootReducer = combineReducers({
   // state: (state = {}) => state
   comments: createCommentReducer,
