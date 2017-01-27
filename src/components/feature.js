a5b7e020a2b052e37349eeaa4b6f702b3c8f0fa5
Olivia Wang <aoli.wl@alibabapictures.com>
2017-01-27 15:43:42 +0800

    store.dispatch

--- src/components/feature.js
+++ src/components/feature.js
@@ -2,14 +2,8 @@
  * Created by aliyy on 2017/1/27.
  */
 import React, { Component } from 'react';
-import { browserHistory } from 'react-router';
 
 export default class Feature extends Component {
-  componentWillMount(){
-    if ( !localStorage.getItem('token') ){
-      browserHistory.push('/');
-    }
-  }
   render(){
     return(
       <div>
--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import App from './components/app';
 import Feature from './components/feature';
 import Index from './components/index'
-
 import reducers from './reducers';
-
 import SignIn from './components/auth/signin';
 import SignOut from './components/auth/signout';
 import SignUp from './components/auth/signup';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/type';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
+// app加载前检查是否有token
+const store = createStoreWithMiddleware(reducers);
+if ( localStorage.getItem('token') ){
+  store.dispatch({ type: AUTH_USER });
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={Index}/>
